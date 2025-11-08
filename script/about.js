c91d28f1b88b6d61e8743b9b51fd31b5c42479b9
Himeshika <himeshikanethmini@gmail.com>
2025-11-08 17:20:19 +0530

    add about section styles

--- script/about.js
+++ script/about.js
@@ -1,7 +1,8 @@
 const aboutSection = document.querySelector("#about");
+
 window.addEventListener("scroll", () => {
     const rect = aboutSection.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 150) {
+    if (rect.top < window.innerHeight - 100) {
         aboutSection.classList.add("visible");
     }
 });
